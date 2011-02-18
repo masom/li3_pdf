845e42f09b2f183a33507bb002b995014b4fa7ca
Martin Samson <pyrolian@gmail.com>
2011-02-18 14:26:42 -0500

    Add support for vanilla upstream TCPDF. Thanks to Nate

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,25 @@
 <?php
 use lithium\core\Libraries;
-Libraries::add(
-	'tcpdf',
-	array(
-		'bootstrap'=> false,
-	)
-);
+// Config for autoloading the TCPDF library
+
+Libraries::add('tcpdf', array(
+	'path' => '/Library/WebServer/Documents/tcpdf',
+	'prefix' => false,
+	'bootstrap' => false,
+	'transform' => function($class, $config) {
+		$map = array(
+			'TCPDF2DBarcode'     => '2dbarcodes',
+			'TCPDFBarcode'       => 'barcodes',
+			'TCPDF_UNICODE_DATA' => 'unicode_data',
+			'PDF417'             => 'pdf417',
+			'TCPDF'              => 'tcpdf',
+			'QRcode'             => 'qrcode'
+		);
+		if (!isset($map[$class])) {
+			return false;
+		}
+		return "{$config['path']}/{$map[$class]}{$config['suffix']}";
+	}
+));
+
 ?>
